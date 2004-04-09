498d419c42c2492a551ec7e42f9af45b491398b9
root <root>
2004-04-09 19:05:23 +0000

    left out a comma in the sql statement ;)

--- setup.php
+++ setup.php
@@ -1,4 +1,4 @@
-<?php /* HELPDESK $Id: setup.php,v 1.2 2004/01/19 21:36:48 mike Exp $ */
+<?php /* HELPDESK $Id: setup.php,v 1.3 2004/04/05 19:15:45 adam Exp $ */
 /*
 dotProject Module
 
@@ -71,8 +71,7 @@ class CSetupHelpDesk {
 			  `item_resolve_custom` int(1) unsigned NOT NULL default '0',
 			  `item_resolved` datetime default NULL,
 			  `item_parent` int(10) unsigned NOT NULL default '0',
-
-        `item_project_id` int(11) NOT NULL default '0'
+        `item_project_id` int(11) NOT NULL default '0',
 			  PRIMARY KEY (item_id)
 			) TYPE=MyISAM
 		";
