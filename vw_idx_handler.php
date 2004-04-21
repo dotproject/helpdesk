4da64b31bd5c57924b920ec3367eacb24d036990
bloaterpaste <bloaterpaste>
2004-04-21 17:54:55 +0000

    Consolidating the requestor email and name, and adjusting the list for the closed items to reflect items that have been resolved today.

--- vw_idx_handler.php
+++ vw_idx_handler.php
@@ -1,4 +1,4 @@
-<?php /* HELPDESK $Id: vw_idx_closed.php,v 1.7 2004/04/21 00:54:16 bloaterpaste Exp $*/
+<?php /* HELPDESK $Id: vw_idx_handler.php,v 1.1 2004/04/21 15:08:55 agorski Exp $*/
 function vw_idx_handler ($opened) {
   global $m, $ipr, $AppUI;
 
@@ -13,22 +13,21 @@ function vw_idx_handler ($opened) {
    */
 
   $sql = "SELECT hi.*,
-          CONCAT(u1.user_first_name,' ',u1.user_last_name) user_fullname,
           CONCAT(u2.user_first_name,' ',u2.user_last_name) assigned_fullname,
-          u1.user_email,
           u2.user_email as assigned_email,
           p.project_id,
           p.project_name,
           p.project_color_identifier
           FROM helpdesk_items hi
-          LEFT JOIN users u1 ON u1.user_id = hi.item_requestor_id
           LEFT JOIN users u2 ON u2.user_id = hi.item_assigned_to
           LEFT JOIN projects p ON p.project_id = hi.item_project_id
-          WHERE (TO_DAYS(NOW()) - TO_DAYS(item_created) = 0) ";
+          WHERE ";
 
   if ($opened) {
+    $sql .= "(TO_DAYS(NOW()) - TO_DAYS(item_created) = 0) ";
     $sql .= "AND item_status = 1";
   } else {
+    $sql .= "(TO_DAYS(NOW()) - TO_DAYS(item_resolved) = 0) ";
     $sql .= "AND item_status = 2";
   }
 
@@ -59,13 +58,6 @@ function vw_idx_handler ($opened) {
     /* We need to check if the user who requested the item is still in the
        system. Just because we have a requestor id does not mean we'll be
        able to retrieve a full name */
-    if ($row["item_requestor_id"]) {
-      $name = $row["user_fullname"] ? $row["user_fullname"] : $row["item_requestor"];
-    } else {
-      $name = $row['item_requestor'];
-    }
-
-    $email = $row["user_email"] ? $row["user_email"] : $row["item_requestor_email"];
 
     if ($opened) {
       if ($row["item_created"]) {
@@ -85,10 +77,10 @@ function vw_idx_handler ($opened) {
       </td>
       <td nowrap=\"nowrap\">
       <?php
-      if ($email) {
-        print "<a href=\"mailto: $email\">$name</a>";
+      if ($row["item_requestor_email"]) {
+        print "<a href=\"mailto: ".$row["item_requestor_email"]."\">".$row['item_requestor']."</a>";
       } else {
-        print $name;
+        print $row['item_requestor'];
       }
       ?>
       </td>
