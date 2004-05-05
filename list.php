a314fbb193a48b1dc510104caa963ca31a444dbf
bloaterpaste <bloaterpaste>
2004-05-05 18:53:36 +0000

    Adding search selectors for Severity, Call Source, Operating System, Application, Assigned To, Requestor, Company, and Project.

--- list.php
+++ list.php
@@ -1,4 +1,4 @@
-<?php /* HELPDESK $Id: list.php,v 1.39 2004/04/29 14:12:07 agorski Exp $ */
+<?php /* HELPDESK $Id: list.php,v 1.40 2004/05/05 15:41:46 bloaterpaste Exp $ */
 
 // check permissions for this module
 $canReadModule = !getDenyRead( $m );
@@ -47,9 +47,57 @@ if (isset( $_GET['item_priority'] )) {
 }
 $priority = $AppUI->getState( 'HelpDeskPriority' ) !== null ? $AppUI->getState( 'HelpDeskPriority' ) : -1;
 
+// check for severity filter
+if (isset( $_GET['item_severity'] )) {
+	$AppUI->setState( 'HelpDeskSeverity', $_GET['item_severity'] );
+}
+$item_severity = $AppUI->getState( 'HelpDeskSeverity' ) !== null ? $AppUI->getState( 'HelpDeskSeverity' ) : -1;
+
+// check for source filter
+if (isset( $_GET['item_source'] )) {
+	$AppUI->setState( 'HelpDeskSource', $_GET['item_source'] );
+}
+$item_source = $AppUI->getState( 'HelpDeskSource' ) !== null ? $AppUI->getState( 'HelpDeskSource' ) : -1;
+
+// check for os filter
+if (isset( $_GET['item_os'] )) {
+	$AppUI->setState( 'HelpDeskOS', $_GET['item_os'] );
+}
+$item_os = $AppUI->getState( 'HelpDeskOS' ) !== null ? $AppUI->getState( 'HelpDeskOS' ) : -1;
+
+// check for application filter
+if (isset( $_GET['item_application'] )) {
+	$AppUI->setState( 'HelpDeskApplication', $_GET['item_application'] );
+}
+$item_application = $AppUI->getState( 'HelpDeskApplication' ) !== null ? $AppUI->getState( 'HelpDeskApplication' ) : -1;
+
+// check for company filter
+if (isset( $_GET['company'] )) {
+	$AppUI->setState( 'HelpDeskCompany', $_GET['company'] );
+}
+$company = $AppUI->getState( 'HelpDeskCompany' ) !== null ? $AppUI->getState( 'HelpDeskCompany' ) : -1;
+
+// check for project filter
+if (isset( $_GET['project'] )) {
+	$AppUI->setState( 'HelpDeskProject', $_GET['project'] );
+}
+$project = $AppUI->getState( 'HelpDeskProject' ) !== null ? $AppUI->getState( 'HelpDeskProject' ) : -1;
+
+// check for assigned_to filter
+if (isset( $_GET['assigned_to'] )) {
+	$AppUI->setState( 'HelpDeskAssignedTo', $_GET['assigned_to'] );
+}
+$assigned_to = $AppUI->getState( 'HelpDeskAssignedTo' ) !== null ? $AppUI->getState( 'HelpDeskAssignedTo' ) : -1;
+
+// check for requestor filter
+if (isset( $_GET['requestor'] )) {
+	$AppUI->setState( 'HelpDeskRequestor', $_GET['requestor'] );
+}
+$requestor = $AppUI->getState( 'HelpDeskRequestor' ) !== null ? $AppUI->getState( 'HelpDeskRequestor' ) : -1;
+
 $tarr = array();
 
-if (isset($search)) {
+if (isset($search)  && strlen($search)>0) {
 	$tarr[] = "(lower(hi.item_title) LIKE lower('%$search%')
               OR lower(hi.item_summary) LIKE lower('%$search%'))";
 }
@@ -64,6 +112,38 @@ if ($status >= 0) {
 
 if ($priority >= 0) {
 	$tarr[] = "hi.item_priority=$priority";
+}
+
+if ($item_severity >= 0) {
+	$tarr[] = "hi.item_severity=$item_severity";
+}
+
+if (isset($item_os)  && strlen($item_os)>0 && $item_os!='-1') {
+	$tarr[] = "hi.item_os='$item_os'";
+}
+
+if (isset($item_application)  && strlen($item_application)>0 && $item_application!='-1') {
+	$tarr[] = "hi.item_application='$item_application'";
+}
+
+if ($item_source >= 0) {
+	$tarr[] = "hi.item_source=$item_source";
+}
+
+if ($company >= 0) {
+	$tarr[] = "hi.item_company_id=$company";
+}
+
+if ($project >= 0) {
+	$tarr[] = "hi.item_project_id=$project";
+}
+
+if ($assigned_to >= 0) {
+	$tarr[] = "hi.item_assigned_to=$assigned_to";
+}
+
+if (isset($requestor)  && strlen($requestor)>0 && $requestor!='-1') {
+	$tarr[] = "hi.item_requestor='$requestor'";
 }
 
 //pull in permitted companies
@@ -103,6 +183,31 @@ if ($orderdesc) {
 //print "<pre>$sql</pre>";
 $rows = db_loadList( $sql );
 
+//get data to populate some of the  drop down selectors
+//assigned to user list
+$sql = "SELECT user_id, CONCAT(user_first_name, ' ', user_last_name)
+        FROM users
+        ORDER BY user_first_name";
+$assigned_to_list = db_loadHashList( $sql );
+//company list
+$sql = "SELECT company_id, company_name
+        FROM companies
+        ORDER BY company_name";
+$company_list = db_loadHashList( $sql );
+//requestor list
+$sql = "SELECT distinct(item_requestor) as requestor, item_requestor
+        FROM helpdesk_items
+        WHERE ".getPermsWhereClause("companies", "item_company_id")."
+        ORDER BY item_requestor";
+$requestor_list = db_loadHashList( $sql );
+//project list
+$sql = "SELECT project_id, project_name
+        FROM projects\n".
+        ((isset($company) && $company>0)?"WHERE project_company=$company\n":"")
+        ."ORDER BY project_name";
+//print "<pre>$sql</pre>";
+$project_list = db_loadHashList( $sql );
+
 // Setup the title block
 $titleBlock = new CTitleBlock( 'Help Desk', 'helpdesk.png', $m, 'ID_HELP_HELPDESK_IDX' );
 
@@ -129,7 +234,7 @@ function changeList() {
   <input type="hidden" name="m" value="<?=$m?>" />
   <input type="hidden" name="a" value="<?=$a?>" />
   <tr>
-    <td><?=$AppUI->_('Search')?>:</td>
+    <td align="right"><?=$AppUI->_('Search')?>:</td>
     <td><input type="text" name="search" class="text" value="<?=$search;?>"></td>
     <td align="right" nowrap><?=$AppUI->_('Call Type')?>:</td>
     <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $ict ), 'item_calltype',
@@ -143,8 +248,45 @@ function changeList() {
     <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $ipr ), 'item_priority',
                                            'size="1" class="text" onchange="changeList()"',
                                            $priority )?></td>
-    <td align="right"><input type="submit" value="<?=$AppUI->_('Search')?>" class="button" /></td>
+    <td align="right" rowspan="2" valign="bottom"><input type="submit" value="<?=$AppUI->_('Search')?>" class="button" /></td>
   </tr>
+  <tr>
+    <td align="right"><?=$AppUI->_('Severity')?>:</td>
+    <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $isv ), 'item_severity',
+                                           'size="1" class="text" onchange="changeList()"',
+                                           $item_severity )?></td>
+    <td align="right" nowrap><?=$AppUI->_('Call Source')?>:</td>
+    <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $ics ), 'item_source',
+                                           'size="1" class="text" onchange="changeList()"',
+                                           $item_source )?></td>
+    <td align="right"><?=$AppUI->_('Operating System')?>:</td>
+    <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $ios ), 'item_os',
+                                           'size="1" class="text" onchange="changeList()"',
+                                           $item_os )?></td>
+    <td align="right"><?=$AppUI->_('Application')?>:</td>
+    <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $iap ), 'item_application',
+                                           'size="1" class="text" onchange="changeList()"',
+                                           $item_application )?></td>
+  </tr>
+  <tr>
+    <td align="right"><?=$AppUI->_('Assigned To')?>:</td>
+    <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $assigned_to_list ), 'assigned_to',
+                                           'size="1" class="text" onchange="changeList()"',
+                                           $assigned_to )?></td>
+    <td align="right"><?=$AppUI->_('Requestor')?>:</td>
+    <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $requestor_list ), 'requestor',
+                                           'size="1" class="text" onchange="changeList()"',
+                                           $requestor )?></td>
+    <td align="right"><?=$AppUI->_('Company')?>:</td>
+    <td><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $company_list ), 'company',
+                                           'size="1" class="text" onchange="changeList()"',
+                                           $company )?></td>
+    <td align="right" nowrap><?=$AppUI->_('Project')?>:</td>
+    <td colspan="2"><?=arraySelect( arrayMerge( array( '-1'=>'All' ), $project_list ), 'project',
+                                           'size="1" class="text" onchange="changeList()"',
+                                           $project )?></td>
+  </tr>
+  
   </form>
 </table>
 <br>
