d5bd55b1d4a4199cb60a240bbcf12ef9169ec5e6
uid157919 <uid157919>
2004-06-04 15:59:35 +0000

    Made filter names into labels for the fields.

--- list.php
+++ list.php
@@ -1,4 +1,4 @@
-<?php /* HELPDESK $Id: list.php,v 1.61 2004/06/02 17:00:01 agorski Exp $ */
+<?php /* HELPDESK $Id: list.php,v 1.62 2004/06/02 17:33:07 bloaterpaste Exp $ */
 
 $HELPDESK_CONFIG = array();
 require_once( "./modules/helpdesk/config.php" );
@@ -42,15 +42,17 @@ if($HELPDESK_CONFIG['search_criteria_search']){
 		$search = $_GET['search'];
 
 		if(strlen(trim($search))>0){
-
 			$tarr[] = "(lower(hi.item_title) LIKE lower('%$search%')
 			      OR lower(hi.item_summary) LIKE lower('%$search%'))";
 		}
 	}
-	$selectors[] = "
-	    <td align=\"right\">".$AppUI->_('Search').":</td>
-	    <td nowrap=\"nowrap\"><input type=\"text\" name=\"search\" class=\"text\" value=\"$search\" size=\"12\">
-                            <input type=\"submit\" value=\"".$AppUI->_('Search')."\" class=\"button\" /></td>";
+	$selectors[] = "<td align=\"right\"><label for=\"search\">"
+               . $AppUI->_('Search')
+               . ":</label></td><td nowrap=\"nowrap\">"
+               . "<input type=\"text\" name=\"search\" id=\"search\" class=\"text\" value=\"$search\" size=\"12\">"
+               . " <input type=\"submit\" value=\""
+               . $AppUI->_('Search')
+               . "\" class=\"button\" /></td>";
 
 }
 
@@ -59,15 +61,21 @@ if($HELPDESK_CONFIG['search_criteria_call_type']){
 	if (isset( $_GET['item_calltype'] )) {
 		$AppUI->setState( 'HelpDeskCallType', $_GET['item_calltype'] );
 	}
+
 	$calltype = $AppUI->getState( 'HelpDeskCallType' ) !== null ? $AppUI->getState( 'HelpDeskCallType' ) : -1;
+
 	if ($calltype >= 0) {
 		$tarr[] = "hi.item_calltype=$calltype";
 	}
-	$selectors[] = "
-	    <td align=\"right\" nowrap>".$AppUI->_('Call Type').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $ict ), 'item_calltype',
-						   'size="1" class="text" onchange="changeList()"',
-						   $calltype )."</td>";
+
+	$selectors[] = "<td align=\"right\" nowrap><label for=\"call_type\">"
+               . $AppUI->_('Call Type')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $ict ),
+                              'item_calltype',
+						                  'size="1" id="call_type" class="text" onchange="changeList()"',
+						                  $calltype )
+               . "</td>";
 }
 
 // check for status filter
@@ -75,15 +83,21 @@ if($HELPDESK_CONFIG['search_criteria_status']){
 	if (isset( $_GET['item_status'] )) {
 		$AppUI->setState( 'HelpDeskStatus', $_GET['item_status'] );
 	}
+
 	$status = $AppUI->getState( 'HelpDeskStatus' ) !== null ? $AppUI->getState( 'HelpDeskStatus' ) : -1;
+
 	if ($status >= 0) {
 		$tarr[] = "hi.item_status=$status";
 	}
-	$selectors[] = "
-	    <td align=\"right\">".$AppUI->_('Status').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $ist ), 'item_status',
-						   'size="1" class="text" onchange="changeList()"',
-						   $status )."</td>";
+
+	$selectors[] = "<td align=\"right\"><label for=\"status\">"
+               . $AppUI->_('Status')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $ist ),
+                              'item_status',
+						                  'size="1" id="status" class="text" onchange="changeList()"',
+						                  $status )
+               . "</td>";
   }
 
 // check for priority filter
@@ -91,15 +105,21 @@ if($HELPDESK_CONFIG['search_criteria_priority']){
 	if (isset( $_GET['item_priority'] )) {
 		$AppUI->setState( 'HelpDeskPriority', $_GET['item_priority'] );
 	}
+
 	$priority = $AppUI->getState( 'HelpDeskPriority' ) !== null ? $AppUI->getState( 'HelpDeskPriority' ) : -1;
+
 	if ($priority >= 0) {
 		$tarr[] = "hi.item_priority=$priority";
 	}
-	$selectors[] = "
-		    <td align=\"right\">".$AppUI->_('Priority').":</td>
-		    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $ipr ), 'item_priority',
-							   'size="1" class="text" onchange="changeList()"',
-							   $priority )."</td>";
+
+	$selectors[] = "<td align=\"right\"><label for=\"priority\">"
+               . $AppUI->_('Priority')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $ipr ),
+                              'item_priority',
+							                'size="1" id="priority" class="text" onchange="changeList()"',
+							                $priority )
+               . "</td>";
 }
 
 // check for severity filter
@@ -107,15 +127,21 @@ if($HELPDESK_CONFIG['search_criteria_severity']){
 	if (isset( $_GET['item_severity'] )) {
 		$AppUI->setState( 'HelpDeskSeverity', $_GET['item_severity'] );
 	}
+
 	$item_severity = $AppUI->getState( 'HelpDeskSeverity' ) !== null ? $AppUI->getState( 'HelpDeskSeverity' ) : -1;
+
 	if ($item_severity >= 0) {
 		$tarr[] = "hi.item_severity=$item_severity";
 	}
-	$selectors[] = "
-	    <td align=\"right\">".$AppUI->_('Severity').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $isv ), 'item_severity',
-						   'size="1" class="text" onchange="changeList()"',
-						   $item_severity )."</td>";
+
+	$selectors[] = "<td align=\"right\"><label for=\"severity\">"
+               . $AppUI->_('Severity')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $isv ),
+                              'item_severity',
+						                  'size="1" id="severity" class="text" onchange="changeList()"',
+						                   $item_severity )
+               . "</td>";
 }
 
 // check for source filter
@@ -123,15 +149,21 @@ if($HELPDESK_CONFIG['search_criteria_call_source']){
 	if (isset( $_GET['item_source'] )) {
 		$AppUI->setState( 'HelpDeskSource', $_GET['item_source'] );
 	}
+
 	$item_source = $AppUI->getState( 'HelpDeskSource' ) !== null ? $AppUI->getState( 'HelpDeskSource' ) : -1;
+
 	if ($item_source >= 0) {
 		$tarr[] = "hi.item_source=$item_source";
 	}
-	$selectors[] = "
-	    <td align=\"right\" nowrap>".$AppUI->_('Call Source').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $ics ), 'item_source',
-						   'size="1" class="text" onchange="changeList()"',
-						   $item_source )."</td>";
+
+	$selectors[] = "<td align=\"right\" nowrap><label for=\"call_source\">"
+               . $AppUI->_('Call Source')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $ics ), 
+                              'item_source',
+						                  'size="1" id="call_source" class="text" onchange="changeList()"',
+						                  $item_source )
+               . "</td>";
 }
 
 // check for os filter
@@ -139,15 +171,21 @@ if($HELPDESK_CONFIG['search_criteria_os']){
 	if (isset( $_GET['item_os'] )) {
 		$AppUI->setState( 'HelpDeskOS', $_GET['item_os'] );
 	}
+
 	$item_os = $AppUI->getState( 'HelpDeskOS' ) !== null ? $AppUI->getState( 'HelpDeskOS' ) : -1;
+
 	if (isset($item_os)  && strlen($item_os)>0 && $item_os!='-1') {
 		$tarr[] = "hi.item_os='$item_os'";
 	}
-	$selectors[] = "
-	    <td align=\"right\">".$AppUI->_('OS').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $ios ), 'item_os',
-						   'size="1" class="text" onchange="changeList()"',
-						   $item_os )."</td>";
+
+	$selectors[] = "<td align=\"right\"><label for=\"os\">"
+               . $AppUI->_('OS')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $ios ),
+                              'item_os',
+						                  'size="1" id="os" class="text" onchange="changeList()"',
+						                  $item_os )
+               . "</td>";
 }
 
 // check for application filter
@@ -155,15 +193,21 @@ if($HELPDESK_CONFIG['search_criteria_application']){
 	if (isset( $_GET['item_application'] )) {
 		$AppUI->setState( 'HelpDeskApplication', $_GET['item_application'] );
 	}
+
 	$item_application = $AppUI->getState( 'HelpDeskApplication' ) !== null ? $AppUI->getState( 'HelpDeskApplication' ) : -1;
+
 	if (isset($item_application)  && strlen($item_application)>0 && $item_application!='-1') {
 		$tarr[] = "hi.item_application='$item_application'";
 	}
-	$selectors[] = "
-	    <td align=\"right\">".$AppUI->_('Application').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $iap ), 'item_application',
-						   'size="1" class="text" onchange="changeList()"',
-						   $item_application )."</td>";
+
+	$selectors[] = "<td align=\"right\"><label for=\"application\">"
+               . $AppUI->_('Application')
+               . "</label>:</td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $iap ),
+                              'item_application',
+						                  'size="1" id="application" class="text" onchange="changeList()"',
+						                  $item_application )
+               . "</td>";
 }
 
 // check for company filter
@@ -171,22 +215,28 @@ if($HELPDESK_CONFIG['search_criteria_company']){
 	if (isset( $_GET['company'] )) {
 		$AppUI->setState( 'HelpDeskCompany', $_GET['company'] );
 	}
+
 	$company = $AppUI->getState( 'HelpDeskCompany' ) !== null ? $AppUI->getState( 'HelpDeskCompany' ) : -1;
+
 	if ($company >= 0) {
 		$tarr[] = "hi.item_company_id=$company";
 	}
 
-	//company list
+	// retrieve company list
 	$sql = "SELECT company_id, company_name
 		FROM companies
     WHERE ".getCompanyPerms("company_id", NULL, PERM_READ)."
 		ORDER BY company_name";
 	$company_list = db_loadHashList( $sql );
-	$selectors[] = "
-	    <td align=\"right\">".$AppUI->_('Company').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $company_list ), 'company',
-							       'size="1" class="text" onchange="changeList()"',
-							       $company )."</td>";
+
+	$selectors[] = "<td align=\"right\"><label for=\"company\">"
+               . $AppUI->_('Company')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $company_list ),
+                              'company',
+							                'size="1" id="company" class="text" onchange="changeList()"',
+							                $company )
+               . "</td>";
 }
 
 // check for project filter
@@ -194,21 +244,28 @@ if($HELPDESK_CONFIG['search_criteria_project']){
 	if (isset( $_GET['project'] )) {
 		$AppUI->setState( 'HelpDeskProject', $_GET['project'] );
 	}
+
 	$project = $AppUI->getState( 'HelpDeskProject' ) !== null ? $AppUI->getState( 'HelpDeskProject' ) : -1;
+
 	if ($project >= 0) {
 		$tarr[] = "hi.item_project_id=$project";
 	}
-	//project list
+
+	// retrieve project list
 	$sql = "SELECT project_id, project_name
 		      FROM projects
           WHERE ".getCompanyPerms("project_company", NULL, PERM_READ)
 		   . "ORDER BY project_name";
 	$project_list = db_loadHashList( $sql );
-	$selectors[] = "
-	    <td align=\"right\">".$AppUI->_('Project').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $project_list ), 'project',
-							       'size="1" class="text" onchange="changeList()"',
-							       $project )."</td>";
+
+	$selectors[] = "<td align=\"right\"><label for=\"project\">"
+               . $AppUI->_('Project')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $project_list ),
+                              'project',
+							                'size="1" id="project" class="text" onchange="changeList()"',
+							                $project )
+               . "</td>";
 }
 
 // check for assigned_to filter
@@ -216,22 +273,28 @@ if($HELPDESK_CONFIG['search_criteria_assigned_to']){
 	if (isset( $_GET['assigned_to'] )) {
 		$AppUI->setState( 'HelpDeskAssignedTo', $_GET['assigned_to'] );
 	}
+
 	$assigned_to = $AppUI->getState( 'HelpDeskAssignedTo' ) !== null ? $AppUI->getState( 'HelpDeskAssignedTo' ) : -1;
+
 	if ($assigned_to >= 0) {
 		$tarr[] = "hi.item_assigned_to=$assigned_to";
 	}
-	//assigned to user list
-	$sql = "SELECT user_id, CONCAT(user_first_name, ' ', user_last_name)
-		FROM users
-    WHERE ".getCompanyPerms("user_company", NULL, PERM_READ, $HELPDESK_CONFIG['the_company'])."
-		ORDER BY user_first_name";
 
+	// retrieve assigned to user list
+	$sql = "SELECT user_id, CONCAT(user_first_name, ' ', user_last_name)
+		      FROM users
+          WHERE ".getCompanyPerms("user_company", NULL, PERM_READ, $HELPDESK_CONFIG['the_company'])."
+		      ORDER BY user_first_name";
 	$assigned_to_list = db_loadHashList( $sql );
-	$selectors[] = "
-	    <td align=\"right\" nowrap>".$AppUI->_('Assigned To').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $assigned_to_list ), 'assigned_to',
-						   'size="1" class="text" onchange="changeList()"',
-						   $assigned_to )."</td>";
+
+	$selectors[] = "<td align=\"right\" nowrap><label for=\"assigned_to\">"
+               . $AppUI->_('Assigned To')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $assigned_to_list ),
+                              'assigned_to',
+						                  'size="1" id="assigned_to" class="text" onchange="changeList()"',
+						                  $assigned_to )
+               . "</td>";
 }
 
 // check for requestor filter
@@ -239,23 +302,28 @@ if($HELPDESK_CONFIG['search_criteria_requestor']){
 	if (isset( $_GET['requestor'] )) {
 		$AppUI->setState( 'HelpDeskRequestor', $_GET['requestor'] );
 	}
+
 	$requestor = $AppUI->getState( 'HelpDeskRequestor' ) !== null ? $AppUI->getState( 'HelpDeskRequestor' ) : -1;
+
 	if (isset($requestor)  && strlen($requestor)>0 && $requestor!='-1') {
 		$tarr[] = "hi.item_requestor='$requestor'";
 	}
-	//requestor list
 
+	// retrieve requestor list
 	$sql = "SELECT distinct(item_requestor) as requestor, item_requestor
-		FROM helpdesk_items
-		WHERE ".getCompanyPerms("item_company_id", NULL, PERM_READ)."
-		ORDER BY item_requestor";
-
+		      FROM helpdesk_items
+		      WHERE ".getCompanyPerms("item_company_id", NULL, PERM_READ)."
+		      ORDER BY item_requestor";
 	$requestor_list = db_loadHashList( $sql );
-	$selectors[] = "
-	    <td align=\"right\">".$AppUI->_('Requestor').":</td>
-	    <td>".arraySelect( arrayMerge( array( '-1'=>'All' ), $requestor_list ), 'requestor',
-						   'size="1" class="text" onchange="changeList()"',
-						   $requestor )."</td>";
+
+	$selectors[] = "<td align=\"right\"><label for=\"requestor\">"
+               . $AppUI->_('Requestor')
+               . ":</label></td><td>"
+               . arraySelect( arrayMerge( array( '-1'=>'All' ), $requestor_list ),
+                              'requestor',
+						                  'size="1" id="requestor" class="text" onchange="changeList()"',
+						                  $requestor )
+               . "</td>";
 }
 
 
@@ -266,8 +334,8 @@ if (count( $tarr )) {
 }
 
 $sql = "SELECT hi.*,
-       CONCAT(u2.user_first_name,' ',u2.user_last_name) assigned_fullname,
-       u2.user_email as assigned_email,
+        CONCAT(u2.user_first_name,' ',u2.user_last_name) assigned_fullname,
+        u2.user_email as assigned_email,
         p.project_id,
         p.project_name,
         p.project_color_identifier
