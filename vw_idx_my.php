08abcf9907cabc124cd0b71d37e654f3843c75e8
cyberhorse <cyberhorse>
2004-08-03 03:27:05 +0000

    Make 'My Items' tab independent (able to show it in other modules, like "today")

--- vw_idx_my.php
+++ vw_idx_my.php
@@ -1,5 +1,7 @@
-<?php /* HELPDESK $Id: vw_idx_my.php,v 1.1 2004/05/05 16:12:47 bloaterpaste Exp $*/
-require_once("vw_idx_handler.php");
+<?php /* HELPDESK $Id: vw_idx_my.php,v 1.2 2004/05/06 13:22:53 agorski Exp $*/
+
+require_once($dPconfig['root_dir'] . "/modules/helpdesk/helpdesk.functions.php");
+require_once($dPconfig['root_dir'] . "/modules/helpdesk/vw_idx_handler.php");
 
 // Show my items
 vw_idx_handler(2);
