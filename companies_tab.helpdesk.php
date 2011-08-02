951533b00ee6a3c0a989bfe56af31511c9629cac
hatax <hatax>
2011-08-02 06:30:22 +0000

    Cleanup for version 1.0.1

--- companies_tab.helpdesk.php
+++
@@ -1,5 +0,0 @@
-<?php /* PROJECTS $Id: companies_tab.helpdesk.php,v 1.1 2005/11/10 22:09:33 pedroix Exp $ */
-global $AppUI, $project_id, $deny, $canRead, $canEdit, $dPconfig;
-
-require dPgetConfig('root_dir') . '/modules/helpdesk/list.php';
-?>
